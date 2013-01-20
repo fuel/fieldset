5a2bd3cc066e3373a441e8ba3cc0f75cbbc12a4b
stevewest <uruwolf@gmail.com>
2013-01-20 12:44:32 +0000

    Adds placeholder Checkbox and Submit classes

---
+++ src/FuelPHP/Fieldset/Input/Submit.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Part of the FuelPHP framework.
+ *
+ * @package   FuelPHP\Fieldset
+ * @version   2.0
+ * @license   MIT License
+ * @copyright 2010 - 2013 Fuel Development Team
+ */
+
+namespace FuelPHP\Fieldset\Input;
+
+/**
+ * 
+ *
+ * @package FuelPHP\Fieldset
+ * @since   2.0.0
+ * @author  Steve "uru" West <uruwolf@gmail.com>
+ */
+class Submit extends Input
+{
+	
+}
