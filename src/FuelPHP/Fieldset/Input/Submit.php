fe2a275c5d8059a8e4a014dbe5840e265fbf3d9c
stevewest <uruwolf@gmail.com>
2013-01-23 22:10:20 +0000

    Updates package tag and adds proper class comments for BasicRender

--- src/FuelPHP/Fieldset/Input/Submit.php
+++ src/FuelPHP/Fieldset/Input/Submit.php
@@ -3,7 +3,7 @@
 /**
  * Part of the FuelPHP framework.
  *
- * @package   FuelPHP\Fieldset
+ * @package   FuelPHP\Fieldset\Input
  * @version   2.0
  * @license   MIT License
  * @copyright 2010 - 2013 Fuel Development Team
--- src/FuelPHP/Fieldset/Render/BasicRender.php
+++ src/FuelPHP/Fieldset/Render/BasicRender.php
@@ -1,11 +1,22 @@
 <?php
 
+/**
+ * Part of the FuelPHP framework.
+ *
+ * @package   FuelPHP\Fieldset\Render
+ * @version   2.0
+ * @license   MIT License
+ * @copyright 2010 - 2013 Fuel Development Team
+ */
+
 namespace FuelPHP\Fieldset\Render;
 
 /**
- * Basic form of a fieldset renderer 
+ * Basic implementation of a fieldset renderer
  *
- * @author Steve West
+ * @package FuelPHP\Fieldset\Render
+ * @since   2.0.0
+ * @author  Fuel Development Team
  */
 class BasicRender implements FieldsetRenderInterface
 {
