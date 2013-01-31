49706796e0f3c807772ba12b65d3bf33873dae79
stevewest <uruwolf@gmail.com>
2013-01-31 12:28:24 +0000

    Adds File and Password input types

---
+++ src/FuelPHP/Fieldset/Input/File.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Part of the FuelPHP framework.
+ *
+ * @package   FuelPHP\Fieldset\Input
+ * @version   2.0
+ * @license   MIT License
+ * @copyright 2010 - 2013 Fuel Development Team
+ */
+
+namespace FuelPHP\Fieldset\Input;
+
+/**
+ * 
+ *
+ * @package FuelPHP\Fieldset\Input
+ * @since   2.0.0
+ * @author  Fuel Development Team
+ */
+class File extends \FuelPHP\Fieldset\Input
+{
+	
+	public function __construct($name = '', array $attributes = array(), $value = null)
+	{
+		$attributes['type'] = 'file';
+		parent::__construct($name, $attributes, $value);
+	}
+	
+}
